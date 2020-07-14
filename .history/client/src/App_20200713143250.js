6683b932e360d6f5d3da37c872a4fa8a18798bde
Sherveer Singh Pannu <sherveersinghpannu@LAPTOP-TUBBIUD9.localdomain>
2020-07-14 10:57:20 +0200

    henter ut access token manuelt og bruker den til å få en json med artist data

---
+++ .history/client/src/App_20200713143250.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <div className="container">
+        <button>login with spotify</button>
+
+      </div>
+
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ .history/client/src/App_20200713143526.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const spotifyLogin = () => {
+    applicationCache.get
+  }
+
+  return (
+    <div className="App">
+      
+      <div className="container">
+        <button onClick={spotifyLogin}>Login with Spotify</button>
+
+      </div>
+
+      
+    </div>
+  );
+}
+
+export default App;
